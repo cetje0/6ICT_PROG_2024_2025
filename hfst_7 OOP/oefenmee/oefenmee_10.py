c630ef98b5af019c95748e7e0a61b0ba534f73df
cetje0 <voce140307@leerling.mosa-rt.be>
2025-05-19 09:04:43 +0200

    added

--- hfst_7 OOP/oefenmee/oefenmee_10.py
+++ hfst_7 OOP/oefenmee/oefenmee_10.py
@@ -1,17 +1,28 @@
 # Maak de klasse Persoon & Hond aan zoals omschreven in oefen mee 10.
 # Je start reeds met de __init__ van beide klassen.
+class Hond:
+    geen_eigenaar = True
+    def __init__(self, naam):
+        self.naam = naam
+        self.eigenaar = ""
 
 class Persoon:
     def __init__(self, naam):
         self.naam = naam
         self.honden = []
+    def koop_hond(self, hond:'Hond')->None:
+        if hond.eigenaar != "":
+            print(f"{hond.naam} heeft reeds {self.naam} als eigenaar.")
+        else:
+            print(f"hond is alleen")
+            self.honden.append(hond)
+            hond.eigenaar = self.naam
 
-
-class Hond:
-    def __init__(self, naam):
-        self.naam = naam
-        self.eigenaar = ""
-
+    def is_eigenaar(self,hond:'Hond')->bool:
+        if hond in self.honden :
+            return True
+        else:
+            return False
 " Via onderstaande code kan je niveau 1 testen. "
 
 # hond_1 = Hond("Lulu")
@@ -27,16 +38,15 @@ class Hond:
 
 
 " Via onderstaande code kan je niveau 2 testen. "
+hond_1 = Hond("Lulu")
+hond_2 = Hond("Floris")
+persoon_1 = Persoon("Jos")
+persoon_2 = Persoon("Jef")
 
-# hond_1 = Hond("Lulu")
-# hond_2 = Hond("Floris")
-# persoon_1 = Persoon("Jos")
-# persoon_2 = Persoon("Jef")
+persoon_1.koop_hond(hond_1)
+persoon_2.koop_hond(hond_2)
+persoon_2.koop_hond(hond_1) # Lulu heeft reeds Jos als eigenaar.
 
-# persoon_1.koop_hond(hond_1)
-# persoon_2.koop_hond(hond_2)
-# persoon_2.koop_hond(hond_1) # Lulu heeft reeds Jos als eigenaar.
-
-# print(persoon_1.is_eigenaar(hond_1)) # True
-# print(persoon_2.is_eigenaar(hond_1)) # False
+print(persoon_1.is_eigenaar(hond_1)) # True
+print(persoon_2.is_eigenaar(hond_1)) # False
 
--- hfst_7 OOP/oefenmee/oefenmee_9.py
+++ hfst_7 OOP/oefenmee/oefenmee_9.py
@@ -1,6 +1,19 @@
 # Maak de klasse Hond aan zoals omschreven in oefen mee 9.
-
-
+import random
+class Hond:
+    locaties:list = ["living", "tuin", "buren", "keuken"]
+    def __init__(self, naam:str)->None:
+        self.naam = naam
+        self.locatie = random.choice(self.locaties)
+        print(f"{self.naam} {self.locatie}")
+    def ziet_hond(self, andere_hond:'Hond')-> None:
+        if self.locatie == andere_hond.locatie:
+            print(f"{self.naam} ziet {andere_hond.naam} in de {self.locatie}.")
+            self.locatie = random.choice(self.locaties)
+            gekozen_hond = random.choice([self, andere_hond])
+            print(f"{gekozen_hond.naam} is bang en rent naar de {self.locatie}.")
+        else:
+             print(f"{self.naam} ziet {andere_hond.naam} niet in de {self.locatie}.")
 
 " Via onderstaande code kan je niveau 1 testen. "
 
@@ -15,11 +28,11 @@
 " Via onderstaande code kan je niveau 2 & 3 testen. Opgelet! Resultaat is random. "
 " Best print je in __init__ ook de locatie waar iedere hond start, zo kan je de werking makkelijker nagaan. "
 
-# hond_1 = Hond("Lulu")
-# hond_2 = Hond("Floris")
-# hond_3 = Hond("Ranger")
+hond_1 = Hond("Lulu")
+hond_2 = Hond("Floris")
+hond_3 = Hond("Ranger")
 
-# hond_1.ziet_hond(hond_2)                  
-# hond_1.ziet_hond(hond_3)   
+hond_1.ziet_hond(hond_2)                  
+hond_1.ziet_hond(hond_3)   
           
-# print(hond_1.locatie)   
+print(hond_1.locatie)   
